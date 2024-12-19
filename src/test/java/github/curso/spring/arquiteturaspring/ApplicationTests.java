11a8c66777a4e791c836f0c1349f67a90a38e29b
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2024-12-19 18:18:18 -0300

    Entendendo a classe Application

---
+++ src/test/java/github/curso/spring/arquiteturaspring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.curso.spring.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
